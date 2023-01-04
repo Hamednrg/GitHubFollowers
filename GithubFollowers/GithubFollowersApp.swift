8c3454ab41f1911fe363a3b7e3e295fe2adbf17d
Hamed Naji <hamednajirg@gmail.com>
2023-01-04 13:17:14 -0500

    Initial Commit

---
+++ GithubFollowers/GithubFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubFollowersApp.swift
+//  GithubFollowers
+//
+//  Created by Hamed Naji on 2023-01-04.
+//
+
+import SwiftUI
+
+@main
+struct GithubFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
