c9aa543df189f6a4d588e8ff1d244b95d5379d5e
Hamed Naji <hamednajirg@gmail.com>
2023-01-04 18:01:38 -0500

    Follower and Following List were added.

---
+++ GithubFollowers/Screens/FollowerListView.swift
@@ -0,0 +1,51 @@
+//
+//  FollowerListView.swift
+//  GithubFollowers
+//
+//  Created by Hamed Naji on 2023-01-04.
+//
+
+import SwiftUI
+
+struct FollowerListView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @StateObject var followerViewModel = FollowerViewModel()
+    @Binding var username: String
+    
+    var body: some View {
+        NavigationView {
+            ZStack{
+                if followerViewModel.allFollowers.isEmpty && !followerViewModel.isLoading {
+                    GFEmptyView(message: "This user doesn't have any follower.")
+                }else {
+                    List{
+                        ForEach(followerViewModel.allFollowers) { follower in
+                            FollowerView(follower: follower)
+                                .onTapGesture {
+                                    Task {
+                                        await followerViewModel.didTapGitHubFollowers(from: follower.login)
+                                        username = follower.login
+                                        dismiss()
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+            .task {
+                await followerViewModel.getFollowers(for: username)
+            }
+            .navigationTitle("Followers")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        
+    }
+}
+
+struct FollowerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowerListView(username: .constant("hamednrg"))
+    }
+}
---
+++ GithubFollowers/Screens/FollowingListView.swift
@@ -0,0 +1,53 @@
+//
+//  FollowingListView.swift
+//  GithubFollowers
+//
+//  Created by Hamed Naji on 2023-01-04.
+//
+
+import Foundation
+import SwiftUI
+
+struct FollowingListView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @StateObject var followingViewModel = FollowingViewModel()
+    @Binding var username: String
+    
+    var body: some View {
+        NavigationView {
+            
+            ZStack{
+                if followingViewModel.allFollowing.isEmpty && !followingViewModel.isLoading {
+                    GFEmptyView(message: "This user doesn't have any following.")
+                } else {
+                    List{
+                        ForEach(followingViewModel.allFollowing) { following in
+                            FollowerView(follower: following)
+                                .onTapGesture {
+                                    Task {
+                                        await followingViewModel.didTapGitHubFollowing(from: following.login)
+                                        username = following.login
+                                        dismiss()
+                                    }
+                                }
+                        }
+                    }
+                    
+                }
+                
+            }
+            .task {
+                await followingViewModel.getFollowing(for: username)
+            }
+            .navigationTitle("Following")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct FollowingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowingListView(username: .constant("hamednrg"))
+    }
+}
